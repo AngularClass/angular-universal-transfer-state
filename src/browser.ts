49368c5291cf37f48059acb8eeeb33b0c60409a4
PatrickJS <github@gdi2290.com>
2017-03-12 22:10:53 -0700

    📦 flatten files

--- src/browser.ts
+++ src/browser.ts
@@ -1,2 +1,22 @@
-export * from './browser-transfer-state.module';
-export * from './transfer-state';
+import { TransferState } from './transfer-state';
+
+export function getTransferState(): TransferState {
+  const win: any = window;
+  const transferState = new TransferState();
+  transferState.initialize(win[TransferState.KEY] || {});
+  return transferState;
+}
+
+@NgModule({
+  providers: [
+    {
+      provide: TransferState,
+      useFactory: getTransferState
+    }
+  ]
+})
+export class BrowserTransferStateModule {
+
+}
+
+export { TransferState } from './transfer-state';
